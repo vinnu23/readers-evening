fa18081b04226173309d3fdbf1dc17c5dce8ab10
vinnu <vinnu@pop-os.localdomain>
2022-06-18 22:26:21 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,10 @@ export default function App() {
       <h1>
         Reader's Evening
       </h1>
-      <h2>Bharath is bakwas fellow</h2>
-      <h2>maybe not</h2>
+      <h2>When life give you lemonade, make lemons</h2>
+      <h3><i>life :</li> What!!</h3>
+
+
       <p>sign up for the event now </p>
       <a href="https://lu.ma/t0zf1srj"><button>Sign UP</button></a>
     </div>

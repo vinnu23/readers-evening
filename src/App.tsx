35500d263221eb2a748c405f8f0cd8fbad8cc720
vinnu <vinnu@pop-os.localdomain>
2022-06-18 22:28:56 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ export default function App() {
       </h1>
       <h2>When life give you lemonade, make lemons</h2>
       <h3><i>life :</i> What!!</h3>
-
+      <div style="width:100%;height:0;padding-bottom:100%;position:relative;"><iframe src="https://giphy.com/embed/PmRQSF9Ce7mM572POK" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/what-really-suprised-PmRQSF9Ce7mM572POK">via GIPHY</a></p>
 
       <p>sign up for the event now </p>
       <a href="https://lu.ma/t0zf1srj"><button>Sign UP</button></a>

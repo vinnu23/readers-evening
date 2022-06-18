385884ec918d6083c904f120b2ece97d7864c86e
vinnu <vinnu@pop-os.localdomain>
2022-06-18 22:23:45 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ export default function App() {
         Reader's Evening
       </h1>
       <h2>Bharath is bakwas fellow</h2>
+      <h2>maybe not</h2>
       <p>sign up for the event now </p>
       <a href="https://lu.ma/t0zf1srj"><button>Sign UP</button></a>
     </div>

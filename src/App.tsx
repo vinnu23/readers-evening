4e36f3b981cf62bcacb4364630c49006520a0dc6
vinnu <vinnu@pop-os.localdomain>
2022-06-21 23:51:49 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import React from 'react'
+import Navbar from './Components/Navbar'
+import Main from './Components/Main'
 
 export default function App() {
   return (
     <div className='continer'>
-      <h1>
-        Reader's Evening
-      </h1>
-      <h2>When life give you lemonade, make lemons</h2>
-      <h3><i>life :</i> What!!</h3>
-      <div style="width:100%;height:0;padding-bottom:100%;position:relative;"><iframe src="https://giphy.com/embed/PmRQSF9Ce7mM572POK" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/what-really-suprised-PmRQSF9Ce7mM572POK">via GIPHY</a></p>
-
-      <p>sign up for the event now </p>
-      <a href="https://lu.ma/t0zf1srj"><button>Sign UP</button></a>
+      <Navbar/>
+      <Main/>
     </div>
   )
 }
---
+++ src/Components/Main.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Main() {
+  return (
+    <div>
+      <h1>Reader's Evening</h1>
+      <h2>When life give you lemonade, make lemons</h2>
+      <h3>
+        <i>life :</i> What!!
+      </h3>
+      <p>sign up for the event now </p>
+      <a href="https://lu.ma/t0zf1srj">
+        <button>Sign UP</button>
+      </a>
+    </div>
+  );
+}
